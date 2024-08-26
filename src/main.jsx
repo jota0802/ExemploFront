63ac599c06ce7ce67cd6f12076810c15e10bc3f5
jota <jvfranco08@gmail.com>
2024-08-26 11:23:33 -0300

    criando as routes error, home e aluno

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,33 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+//instalando as dependencias  
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import App from './App.jsx'
+import Error from './routes/Error.jsx'
+import Home from './routes/Home.jsx'
+import Aluno from './routes/Aluno,.jsx'
+
+
+
+//criando a funcao Browser Router
+const router = createBrowserRouter([
+  {
+    //Chamando elemento pai 
+
+    path:'/',element:<App/>,
+    errorElement:<Error/>,
+
+    //Chamando elemento filho
+    children:[
+      {path:'/',element:<Home/>},
+      {path: '/aluno',element:<Aluno/>}
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
---
+++ src/routes/Aluno,.jsx
@@ -0,0 +1,8 @@
+const Aluno =()=>{
+    return(
+        <>
+        <h1>lista de alunos</h1>
+        </>
+    )
+}
+export default Aluno
